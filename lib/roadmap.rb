e7e7d661e4f178a07539c8f962f153637e36c0ff
Devin Tassie <devintassie@gmail.com>
2018-07-04 16:09:15 +0000

    ability to get the number of remaining checkpoints complete

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -15,5 +15,17 @@ module Roadmap
         response = self.class.get("/checkpoints/#{checkpoint_id}", headers: { "authorization" => @auth_token})
         @checkpoint = JSON.parse(response.body)
     end
+    
+    def get_checkpoint(checkpoint_id)
+      response = self.class.get(api_url("checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
+      @checkpoints = JSON.parse(response.body)
+    end
+
+    def get_remaining_checkpoints(chain_id)
+    #chain id = 6698
+      response = self.class.get(api_url("enrollment_chains/#{chain_id}/checkpoints_remaining_in_section"), headers: { "authorization" => @auth_token })
+      @remaining_checkpoints = JSON.parse(response.body)
+    end
+
 
 end
